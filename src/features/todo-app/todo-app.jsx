6bb5f148214a0ddf32f6d492457f26feb8ac8dec
Zaza Vanderquack <aurelien.strimelle@bstorm.be>
2025-07-29 16:02:52 +0200

    Component TodoApp 🌟

--- src/features/todo-app/todo-app.jsx
+++ src/features/todo-app/todo-app.jsx
@@ -1,15 +1,35 @@
+import { nanoid } from "nanoid";
 import TaskForm from "../../components/task-form/task-form";
 import TaskList from "../../components/task-list/task-list";
+import { useState } from "react";
 
 export default function TodoApp() {
+
+    const [tasks, setTasks] = useState([])
+    
+    const handleAddTask = (data) => {
+        const task = {
+            id: nanoid(),
+            name: data.name,
+            desc: data.desc,
+            priority: data.priority,
+            isDone: false
+
+        }
+        setTasks(prevTask => [...prevTask, task])
+    }
+
+    const handleDeleteTask = (id) => {
+        setTasks(prevTask => prevTask.filter((element) => element.id !== id))
+    }
 
     return (
         <div>
             <h2>Ajouter une tache</h2>
-            <TaskForm />
+            <TaskForm onSubmitCallback={handleAddTask}/>
 
             <h2>Liste des taches</h2>
-            <TaskList />
+            <TaskList tasks={tasks} onDeleteCallback={handleDeleteTask}/>
         </div>
     )
 } 
